cfad89678c28451378d032bbd529512d7076b371
j94wittwer <j94wittwer@gmail.com>
2023-05-12 11:27:17 +0200

    Implemented Login logic

--- src/main/java/LoginDelocalized/LoginController.java
+++ src/main/java/LoginDelocalized/LoginController.java
@@ -6,19 +6,29 @@ public class LoginController {
     private UserRepository userRepository;
 
     public LoginController(AuthenticationService authenticationService, UserRepository userRepository) {
-
+        this.authenticationService = authenticationService;
+        this.userRepository = userRepository;
     }
 
     public boolean authenticateUser(String username, String password) {
-
+        User user = userRepository.getUserByUsername(username);
+        boolean isAuthenticated = false;
+        if (user != null) {
+            isAuthenticated = authenticationService.authenticate(user, username, password);
+        } else {
+            System.out.println("User does not exist.");
+        }
+        return isAuthenticated;
     }
 
     public void createUser(String username, String password) {
-
+        User newUser = authenticationService.createUser(username, password);
+        userRepository.addUser(newUser);
     }
 
     public void changePassword(String username, String oldPassword, String newPassword) {
-
+        User user = userRepository.getUserByUsername(username);
+        authenticationService.changePassword(user, oldPassword, newPassword);
     }
 
 }
--- src/main/java/LoginDelocalized/UserRepository.java
+++ src/main/java/LoginDelocalized/UserRepository.java
@@ -8,18 +8,33 @@ public class UserRepository {
     private List<User> users;
 
     public UserRepository() {
-
+        this.users = new ArrayList<>();
     }
 
     public void addUser(User user) {
-
+        if (!users.contains(user)) {
+            users.add(user);
+        } else {
+            System.out.println("User is already in repository.");
+        }
     }
 
     public User getUserByUsername(String username) {
-
+        for (User user : users) {
+            if (user.getName() == username) {
+                return user;
+            }
+        }
+        return null;
     }
 
     public void removeUser(User user) {
-
+        if (users.contains(user)) {
+            users.remove(user);
+        } else {
+            System.out.println("User is not existent in repository.");
+        }
     }
+
+
 }
