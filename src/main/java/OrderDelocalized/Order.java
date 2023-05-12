780f78baa0a947e55796ebe535026a987167cdf1
j94wittwer <j94wittwer@gmail.com>
2023-05-12 13:00:57 +0200

    Implemented Order task

--- src/main/java/OrderDelocalized/Order.java
+++ src/main/java/OrderDelocalized/Order.java
@@ -7,15 +7,25 @@ public class Order {
     private Map<String, Integer> products;
 
     public Order(Customer customer, Map<String, Integer> products) {
-
+        this.customer = customer;
+        this.products = products;
     }
 
     public double calculatePrice() {
-
+        double totalPrice = 0;
+        for (Map.Entry<String, Integer> product : products.entrySet()) {
+            totalPrice += product.getValue();
+        }
+        return totalPrice;
     }
 
     public double calculateDiscountPrice() {
-
+        double discountPrice = 0;
+        double discountRate = 0.1;
+        for (Map.Entry<String, Integer> product : products.entrySet()) {
+            discountPrice += product.getValue() * (1 - discountRate);
+        }
+        return discountPrice;
     }
 
 }
