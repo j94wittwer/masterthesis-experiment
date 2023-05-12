941eb9ff864b6f746490fdbb84a70b8891d68901
j94wittwer <j94wittwer@gmail.com>
2023-05-12 11:14:55 +0200

    created class/method skeletons for tasks

--- src/main/java/LoginDelocalized/AuthenticationService.java
+++ src/main/java/LoginDelocalized/AuthenticationService.java
@@ -3,29 +3,19 @@ package LoginDelocalized;
 public class AuthenticationService {
 
     public User createUser(String username, String password) {
-        return new User(username, password);
+
     }
 
     public User changeUsername(User user, String newUsername) {
-        if (newUsername == null || newUsername.isEmpty() || newUsername.trim().isEmpty()) {
-            System.out.println("Please provide valid Username");
-        } else {
-            user.setName(newUsername);
-        }
-        return user;
+
     }
 
     public void changePassword(User user, String oldPassword, String newPassword) {
-        if (authenticate(user, user.getName(), oldPassword)) {
-            user.setPassword(newPassword);
-            System.out.println("Password changed successfully");
-        } else {
-            System.out.println("Old password does not match");
-        }
+
     }
 
     public boolean authenticate(User user, String password, String username) {
-        return user.getPassword() == password && user.getName() == username;
+
     }
 
 
