e3d881511762546fab92c200d6a4bfc74b7a9b6d
david <david.alexander@gmx.ch>
2023-06-07 07:47:10 +0200

    before

--- src/main/java/EmptyTestTask/DiceValue.java
+++ src/main/java/EmptyTestTask/DiceValue.java
@@ -13,34 +13,35 @@ public enum DiceValue {
     FIVE,
     SIX;
 
-    public static DiceValue getRandomRollValue(){
+    public static DiceValue getRandomRollValue() {
         Random random = new Random();
         DiceValue roll = null;
-        while(roll == null){
+        while (roll == null) {
             roll = values()[random.nextInt(values().length)];
-            if(roll == DiceValue.UNROLLED || roll == DiceValue.REMOVED){
+            if (roll == DiceValue.UNROLLED || roll == DiceValue.REMOVED) {
                 roll = null;
             }
         }
         return roll;
     }
-    public static int diceValueToInt(DiceValue value){
-        if(value == DiceValue.ONE){
+
+    public static int diceValueToInt(DiceValue value) {
+        if (value == DiceValue.ONE) {
             return 1;
         }
-        if(value == DiceValue.TWO){
+        if (value == DiceValue.TWO) {
             return 2;
         }
-        if(value == DiceValue.THREE){
+        if (value == DiceValue.THREE) {
             return 3;
         }
-        if(value == DiceValue.FOUR){
+        if (value == DiceValue.FOUR) {
             return 4;
         }
-        if(value == DiceValue.FIVE){
+        if (value == DiceValue.FIVE) {
             return 5;
         }
-        if(value == DiceValue.SIX){
+        if (value == DiceValue.SIX) {
             return 6;
         }
         throw new InvalidParameterException();
